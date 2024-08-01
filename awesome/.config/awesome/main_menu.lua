4f5dd6c406a677c24cba4816c4a2290d4f3c86fe
Breno Salles <git@brenosalles.com>
2024-08-01 19:42:46 +0100

    feat: Improve awesome main menu to include logout logic

--- awesome/.config/awesome/main_menu.lua
+++ awesome/.config/awesome/main_menu.lua
@@ -7,30 +7,69 @@ local global = require("global")
 
 -- Create a launcher widget and a main menu
 local myawesomemenu = {
-	{ "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
-	{ "manual", global.terminal_cmd .. "man awesome" },
-	{ "edit config", global.editor_cmd .. " " .. awesome.conffile },
-	{ "restart", awesome.restart },
-	{ "quit", function() awesome.quit() end },
+	{
+		"hotkeys",
+		function() hotkeys_popup.show_help(nil, awful.screen.focused()) end
+	},
+	{
+		"open terminal", global.terminal
+	},
+	{
+		"manual",
+		global.terminal_cmd .. "man awesome"
+	},
+	{
+		"edit config",
+		global.editor_cmd .. " " .. awesome.conffile
+	},
+	{
+		"restart", awesome.restart
+	}
 }
+local menu_awesome = { "Awesome", myawesomemenu }
 
-local menu_awesome = { "awesome", myawesomemenu, beautiful.awesome_icon }
-local menu_terminal = { "open terminal", global.terminal }
+local ICON_DIR = os.getenv("HOME") .. '/.config/awesome/awesome-wm-widgets/logout-menu-widget/icons/'
+local logout_menu = {
+	{
+		"Log out",
+		function() awesome.quit() end,
+		ICON_DIR .. "log-out.svg"
+	},
+	{
+		"Lock",
+		function() awful.spawn.with_shell("light-locker-command --lock") end,
+		ICON_DIR .. "lock.svg"
+	},
+	{
+		"Reboot",
+		function() awful.spawn.with_shell("reboot") end,
+		ICON_DIR .. "refresh-cw.svg"
+	},
+	{
+		"Suspend",
+		function() awful.spawn.with_shell("systemctl suspend") end,
+		ICON_DIR .. "moon.svg"
+	},
+	{
+		"Power off",
+		function() awful.spawn.with_shell("shutdown now") end,
+		ICON_DIR .. "power.svg"
+	},
+}
+local menu_logout = { "System", logout_menu, ICON_DIR .. "power_w.svg" }
 
-local mymainmenu = {}
+local mymainmenu = awful.menu({
+		items = {
+			menu_logout,
+				{ "Debian", debian.menu.Debian_menu.Debian },
+			menu_awesome
+		}
+	})
 
 if has_fdo then
 	mymainmenu = freedesktop.menu.build({
-		before = { menu_awesome },
-		after =  { menu_terminal }
-	})
-else
-	mymainmenu = awful.menu({
-		items = {
-			menu_awesome,
-				{ "Debian", debian.menu.Debian_menu.Debian },
-			menu_terminal,
-		}
+		after = { menu_awesome },
+		before =  { menu_logout }
 	})
 end
 
