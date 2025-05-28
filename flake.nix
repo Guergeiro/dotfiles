c777d2d55dc08dde6d27f926cbc0ec1d34b44603
Breno Salles <git@brenosalles.com>
2025-05-28 22:59:40 +0100

    feat: Initial home-manager

--- flake.nix
+++ flake.nix
@@ -3,19 +3,42 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }: {
-    homeManagerModules.default = {
-      config,
-      pkgs,
-      ...
-    }: {
-      home.shellAliases = {
-        foo = "echo bar";
+  outputs = { self, nixpkgs, home-manager, ... }:
+  let
+    linuxSystem = "x86_64-linux";
+    darwinSystem = "aarch64-darwin";
+  in
+  {
+    homeConfigurations."breno-linux" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.${linuxSystem};
+
+      # Specify your home configuration modules here, for example,
+      # the path to your home.nix.
+      modules = [ ./home.nix ];
+
+      # Optionally use extraSpecialArgs
+      # to pass through arguments to home.nix
+      extraSpecialArgs = {
+        username = "breno";
+        system = linuxSystem;
+      };
+    };
+    homeConfigurations."breno-macos" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.${darwinSystem};
+
+      # Specify your home configuration modules here, for example,
+      # the path to your home.nix.
+      modules = [ ./home.nix ];
+
+      # Optionally use extraSpecialArgs
+      # to pass through arguments to home.nix
+      extraSpecialArgs = {
+        username = "breno";
+        system = darwinSystem;
       };
     };
   };
