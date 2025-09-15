408db26feb3c8798b788fc2f63328f7a7444ffac
Breno Salles <git@brenosalles.com>
2025-09-15 08:46:38 +0100

    feat(ssh): Solve deprecated top level option

--- ssh/default.nix
+++ ssh/default.nix
@@ -2,6 +2,19 @@
 {
   programs.ssh = {
     enable = true;
-    addKeysToAgent = "yes";
+    enableDefaultConfig = false;
+    matchBlocks."*" = {
+      forwardAgent = false;
+      compression = false;
+      serverAliveInterval = 0;
+      serverAliveCountMax = 3;
+      hashKnownHosts = false;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
+
+      addKeysToAgent = "yes";
+    };
   };
 }
