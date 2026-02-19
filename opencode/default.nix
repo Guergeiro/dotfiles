5f23f7bb380a17bb81104f7891c8fae4116c6f98
Breno Salles <git@brenosalles.com>
2026-02-19 11:05:37 +0000

    feat(opencode): Add terraform and gh_grep mcp servers

--- opencode/default.nix
+++ opencode/default.nix
@@ -11,9 +11,25 @@
         bash = "allow";
       };
       mcp = {
-        "astro-mcp-server" = {
+        "astro" = {
           type = "remote";
           url = "https://mcp.docs.astro.build/mcp";
+          enabled = true;
+        };
+        "gh_grep" = {
+          type = "remote";
+          url = "https://mcp.grep.app";
+          enabled = true;
+        };
+        "terraform" = {
+          type = "local";
+          command = [
+            "docker"
+            "run"
+            "-i"
+            "--rm"
+            "hashicorp/terraform-mcp-server:0.4.0"
+          ];
           enabled = true;
         };
       };
