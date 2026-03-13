4e696f42a652e7991d161c35d545f4f18f2329a6
Breno Salles <git@brenosalles.com>
2026-03-13 23:33:24 +0000

    feat(rofi): Extra configs

--- rofi/default.nix
+++ rofi/default.nix
@@ -3,5 +3,11 @@
   programs.rofi = {
     enable = true;
     theme = "${rofi-dracula}/theme/config1.rasi";
+    font = "FantasqueSansM Nerd Font";
+    extraConfig = {
+      matching = "fuzzy";
+      sort-method = "fzf";
+      sort = true;
+    };
   };
 }
