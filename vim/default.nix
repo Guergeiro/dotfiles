06341210f6c0db902729a972d9a30464f0d3d20c
Breno Salles <git@brenosalles.com>
2026-03-23 22:28:29 +0000

    feat(vim): Use home-manager for neovim base configuration

--- vim/default.nix
+++ vim/default.nix
@@ -9,14 +9,29 @@ let
   vimDir = "${dotfilesDir}/vim/vim";
 in
 {
-  home.file.".vimrc".source = config.lib.file.mkOutOfStoreSymlink "${vimDir}/.vimrc";
-  home.file.".vimrc".force = true;
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    withPython3 = true;
+    withNodeJs = true;
+    withPerl = true;
+    withRuby = true;
+  };
+  home.packages = with pkgs; [
+    tree-sitter
+    nixfmt
+  ];
 
   home.file.".config/nvim/" = {
     source = config.lib.file.mkOutOfStoreSymlink "${vimDir}/.config/nvim/";
     recursive = true;
     force = true;
   };
+  home.file.".vimrc".source = config.lib.file.mkOutOfStoreSymlink "${vimDir}/.vimrc";
+  home.file.".vimrc".force = true;
   home.file.".vim/" = {
     source = config.lib.file.mkOutOfStoreSymlink "${vimDir}/.vim/";
     recursive = true;
