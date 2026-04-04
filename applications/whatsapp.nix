f37dba9afb888e8739ef22777e954b5944012a6f
Breno Salles <git@brenosalles.com>
2026-04-04 18:38:09 +0100

    feat: Add whatsapp icon

--- applications/whatsapp.nix
+++ applications/whatsapp.nix
@@ -13,6 +13,10 @@ in
   xdg.desktopEntries.whatsapp = lib.mkIf enable {
     name = "WhatsApp";
     exec = "${browser} --app=https://web.whatsapp.com/";
+    icon = pkgs.fetchurl {
+      url = "https://static.whatsapp.net/rsrc.php/yp/r/iBj9rlryvZv.svg";
+      sha256 = "sha256-QmNVdkQ5QwVITocMZ8Hlf26P559OdrCJO6ezPoMKUeI=";
+    };
   };
 
   programs.chromium = {
