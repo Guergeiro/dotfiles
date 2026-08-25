a06b6a89b7fd2c249a53330528e80bf2ffae45cd
Breno Salles <git@brenosalles.com>
2026-08-25 23:12:23 +0100

    feat(opencode): Add nix mcp server and improve conditional mcp servers

--- cli/opencode.nix
+++ cli/opencode.nix
@@ -4,12 +4,10 @@
   envVars,
   opencode-skills,
   opencode-dracula,
+  pkgs,
   ...
 }:
 let
-  # TODO: Refactor to use the following:
-  # lib.mkIf (lib.all (attr: envVars ? ${attr}) [ "JENKINS_URL" "JENKINS_USERNAME" "JENKINS_PASSWORD" ]) {
-
   mcpServers = lib.mkMerge [
     {
       "sequential" = {
@@ -39,55 +37,73 @@ let
         url = "https://mcp.grep.app";
         enabled = true;
       };
+      "nix" = {
+        type = "local";
+        command = [ "${pkgs.mcp-nixos}/bin/mcp-nixos" ];
+        enabled = true;
+      };
     }
-    (lib.mkIf (envVars ? JENKINS_URL && envVars ? JENKINS_USERNAME && envVars ? JENKINS_PASSWORD) {
-      "jenkins" = {
-        type = "local";
-        command = [
-          "docker"
-          "run"
-          "-i"
-          "--rm"
-          "--pull=always"
-          "-e"
-          "JENKINS_URL"
-          "-e"
-          "JENKINS_USERNAME"
-          "-e"
-          "JENKINS_PASSWORD"
-          "artifactory.boschdevcloud.com/sh-docker-local/mcp-jenkins:latest"
-        ];
-        environment = {
-          JENKINS_URL = envVars.JENKINS_URL;
-          JENKINS_USERNAME = envVars.JENKINS_USERNAME;
-          JENKINS_PASSWORD = envVars.JENKINS_PASSWORD;
+    (lib.mkIf
+      (lib.all (attr: envVars ? ${attr}) [
+        "JENKINS_URL"
+        "JENKINS_USERNAME"
+        "JENKINS_PASSWORD"
+      ])
+      {
+        "jenkins" = {
+          type = "local";
+          command = [
+            "docker"
+            "run"
+            "-i"
+            "--rm"
+            "--pull=always"
+            "-e"
+            "JENKINS_URL"
+            "-e"
+            "JENKINS_USERNAME"
+            "-e"
+            "JENKINS_PASSWORD"
+            "artifactory.boschdevcloud.com/sh-docker-local/mcp-jenkins:latest"
+          ];
+          environment = {
+            JENKINS_URL = envVars.JENKINS_URL;
+            JENKINS_USERNAME = envVars.JENKINS_USERNAME;
+            JENKINS_PASSWORD = envVars.JENKINS_PASSWORD;
+          };
+          enabled = true;
         };
-        enabled = true;
-      };
-    })
-    (lib.mkIf (envVars ? SONARQUBE_TOKEN && envVars ? SONARQUBE_URL) {
-      "sonar" = {
-        type = "local";
-        command = [
-          "docker"
-          "run"
-          "-i"
-          "--rm"
-          "--pull=always"
-          "--init"
-          "-e"
-          "SONARQUBE_TOKEN"
-          "-e"
-          "SONARQUBE_URL"
-          "mcp/sonarqube:latest"
-        ];
-        environment = {
-          SONARQUBE_TOKEN = envVars.SONARQUBE_TOKEN;
-          SONARQUBE_URL = envVars.SONARQUBE_URL;
+      }
+    )
+    (lib.mkIf
+      (lib.all (attr: envVars ? ${attr}) [
+        "SONARQUBE_TOKEN"
+        "SONARQUBE_URL"
+      ])
+      {
+        "sonar" = {
+          type = "local";
+          command = [
+            "docker"
+            "run"
+            "-i"
+            "--rm"
+            "--pull=always"
+            "--init"
+            "-e"
+            "SONARQUBE_TOKEN"
+            "-e"
+            "SONARQUBE_URL"
+            "mcp/sonarqube:latest"
+          ];
+          environment = {
+            SONARQUBE_TOKEN = envVars.SONARQUBE_TOKEN;
+            SONARQUBE_URL = envVars.SONARQUBE_URL;
+          };
+          enabled = true;
         };
-        enabled = true;
-      };
-    })
+      }
+    )
   ];
 in
 {
