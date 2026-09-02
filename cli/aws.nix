bbf07ea7520772a5bcef7d84de81888bcb61272e
Breno Salles <git@brenosalles.com>
2026-09-02 10:55:22 +0100

    fix(aws): SSO handling was not working

--- cli/aws.nix
+++ cli/aws.nix
@@ -7,23 +7,17 @@
 {
   programs.awscli = {
     enable = isWork;
-    settings = lib.mkMerge [
-      (lib.mapAttrs' (name: p: {
+    settings = (
+      lib.mapAttrs' (name: p: {
         name = "profile ${name}";
         value = {
-          region = p.region;
+          sso_region = awsCli.sso.region;
           sso_account_id = p.account_id;
           sso_role_name = p.role;
-          sso_session = "aws";
-        };
-      }) awsCli.profiles)
-      {
-        "sso-session aws" = {
           sso_start_url = awsCli.sso.start_url;
-          sso_region = awsCli.sso.region;
-          sso_registration_scopes = "sso:account:access";
+          region = p.region;
         };
-      }
-    ];
+      }) awsCli.profiles
+    );
   };
 }
